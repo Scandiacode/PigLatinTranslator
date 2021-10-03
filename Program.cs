4e32da664823c798ead85773e89c4953a7d6efe8
Logan Harris <logan.harris41@yahoo.com>
2021-10-03 10:54:48 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PigLatinTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Pig Latin Translator");
+
+            Console.WriteLine("Please Enter a Statment to be Translated into Pig Latin:");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine(VowelMethod(userInput.ToLower()));
+        }
+
+        public static string VowelMethod(string wordPig)
+        {
+            string wordWay = "way";
+            string wordAy = "ay";
+            char[] mainVowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            foreach (char letters in mainVowels)
+            {
+                if (wordPig[0] == letters)
+                {
+                    return wordPig + wordWay;
+                }
+            }
+
+            int startervowel = wordPig.IndexOfAny(mainVowels);
+            string sentChunk = wordPig.Substring(startervowel);
+            string sentChunk2 = wordPig.Substring(0, startervowel);
+
+            return sentChunk + sentChunk2 + wordAy;
+        }
+    }
+}
