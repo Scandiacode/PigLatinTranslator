7b88448ecd0860de5d4e6091733197ecc23d25e4
Logan Harris <logan.harris41@yahoo.com>
2021-10-03 23:41:22 -0400

    basic finish

--- Program.cs
+++ Program.cs
@@ -6,12 +6,24 @@ namespace PigLatinTranslator
     {
         static void Main(string[] args)
         {
+            bool runProgram = true;
+
             Console.WriteLine("Welcome to the Pig Latin Translator");
 
-            Console.WriteLine("Please Enter a Statment to be Translated into Pig Latin:");
-            string userInput = Console.ReadLine();
+            while (runProgram == true)
+            {
+                Console.WriteLine("Please Enter a Statment to be Translated into Pig Latin:");
+                string userInput = Console.ReadLine();
 
-            Console.WriteLine(VowelMethod(userInput.ToLower()));
+                Console.WriteLine(VowelMethod(userInput.ToLower()));
+
+                Console.WriteLine("Do you want to translate another word? (y/n)");
+                string loopAnswer = Console.ReadLine();
+                if (loopAnswer == "n")
+                {
+                    runProgram = false;
+                }
+            }
         }
 
         public static string VowelMethod(string wordPig)
